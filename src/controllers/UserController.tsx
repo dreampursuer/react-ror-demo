a60267661b96e760295d8204c78866e646710cdc
jien <>
2023-01-16 21:08:11 +0800

    skipAccessCheck

--- src/controllers/UserController.tsx
+++ src/controllers/UserController.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import {Show} from "../views/user/show";
 import {Login} from "../views/user/login";
 import {setLoginUser} from "../conf/ApplicationConfig";
-import {accessAll, redirectTo} from "react-ror";
+import {skipAccessCheck, redirectTo} from "react-ror";
 
 export class UserController{
     public show(){
         return (<Show />)
     }
 
-    @accessAll
+    @skipAccessCheck
     public login(){
         return <Login />
     }
